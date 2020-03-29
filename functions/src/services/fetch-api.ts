1ad79f60b85f2750d40438e2b1c8457f9bcc4860
Vadym Parakonnyi <v.parakonnyi@gmail.com>
2020-03-29 14:40:19 +0300

    add fallback intent, optimize  code

--- functions/src/services/fetch-api.ts
+++ functions/src/services/fetch-api.ts
@@ -1,27 +1,20 @@
 import fetch from 'node-fetch';
 import { Parser } from 'xml2js';
 import { Station, NowPlayingInfo } from '../models';
-import { parseTitleBy } from '../helpers';
 
 const parser = new Parser();
 
 export async function fetchAristocratsApi(
   station: Station = Station.Aristocrats
 ): Promise<NowPlayingInfo> {
-  const data: NowPlayingInfo = {
-    artist: 'undefined song',
-    song: 'undefined artist',
-  };
+  let info = new NowPlayingInfo();
   const nowplayingUrl = `https://aristocrats.fm/service/nowplaying-${station}8.xml`;
   const response = await fetch(nowplayingUrl).catch(() => {
     console.log('Error in fetching NowPlaying');
   });
   if (response) {
-    const xmlData = await response.text();
-    const rawData = await parser.parseStringPromise(xmlData);
-    // console.log({ rawData });
-    data.artist = parseTitleBy(rawData, 'artist');
-    data.song = parseTitleBy(rawData, 'song');
+    const parsedData = await parser.parseStringPromise(await response.text());
+    info = new NowPlayingInfo(parsedData);
   }
-  return data;
+  return info;
 }
