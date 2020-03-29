91be5e619bfaaca5858272d3ed29737db1961261
Vadym Parakonnyi <v.parakonnyi@gmail.com>
2020-03-29 14:42:43 +0300

    Develop (#1)
    * add  travis
    
    * add fallback intent, optimize  code

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,15 +5,30 @@ import { Station } from './models';
 import { fetchAristocratsApi } from './services';
 import { getMessage } from './helpers';
 
-const app = dialogflow({ debug: true });
+const WELCOME_INTENT = 'Default Welcome Intent';
+const STATION_INTENT = 'Station Number';
 
-app.intent('Default Welcome Intent', async conv => {
-  const { station } = conv.parameters;
-  // Complete your fulfillment logic and
-  // send a response when the function is done executing
-  const res = await fetchAristocratsApi(station as Station);
-  const message = getMessage(res);
-  conv.close(message);
+const app = dialogflow({ debug: true });
+// you can add a fallback function instead of a function for individual intents
+app.fallback(async conv => {
+  // intent contains the name of the intent
+  // you defined in the Intents area of Dialogflow
+  const intent = conv.intent;
+  let message = `There is nothing playing at the moment. You can check again later.
+  Thank you!`;
+  switch (intent) {
+    case WELCOME_INTENT:
+      message = await handler();
+      conv.close(message);
+      break;
+    case STATION_INTENT:
+      message = await handler();
+      conv.ask('What station are you listening');
+      break;
+    default:
+      message = await handler();
+      conv.close(message);
+  }
 });
 
 /* export const _nowplaying = functions.https.onCall(async (data = {}, context) => {
@@ -23,5 +38,11 @@ app.intent('Default Welcome Intent', async conv => {
     message: getMessage(res),
   };
 }); */
+
+async function handler(station = Station.Aristocrats) {
+  const res = await fetchAristocratsApi(station);
+  const message = getMessage(res);
+  return message;
+}
 
 export const nowplaying = functions.https.onRequest(app);
--- functions/src/models/nowplaying.ts
+++ functions/src/models/nowplaying.ts
@@ -1,4 +1,13 @@
-export interface NowPlayingInfo {
-  artist: string | undefined;
-  song: string | undefined;
+import { parseTitleBy } from '../helpers';
+
+export class NowPlayingInfo {
+  artist = 'undefined artist';
+  song = 'undefined song';
+
+  constructor(xmlData?: any) {
+    if (xmlData) {
+      this.artist = parseTitleBy(xmlData, 'artist');
+      this.song = parseTitleBy(xmlData, 'song');
+    }
+  }
 }
